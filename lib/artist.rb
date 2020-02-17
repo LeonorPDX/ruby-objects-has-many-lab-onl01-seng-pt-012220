f0e9902b552b22a47819ae9d059d89a16136f577
Leonor Colbert <leonoracolbert@gmail.com>
2020-02-17 19:32:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,6 @@ class Artist
   
   def initialize(name)
     @name = name
-    @songs = []
   end
   
   def add_song(song)
@@ -12,7 +11,7 @@ class Artist
   end
   
   def songs
-    @songs
+    Song.all.select {|song| song.artist == self}
   end
   
 end
